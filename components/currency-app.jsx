97a18b2ab495982208e8d2fd2acda3b3b59d0161
willmcneilly <willmcneilly@me.com>
2015-06-21 13:58:18 +0100

    find endpoint for currency rates

--- components/currency-app.jsx
+++ components/currency-app.jsx
@@ -1,5 +1,7 @@
 'use strict';
 
+//https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDMXN%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
+
 var React = require('react/addons');
 var TopBar = require('./top-bar');
 var CurrencyNode = require('./currency-node');
